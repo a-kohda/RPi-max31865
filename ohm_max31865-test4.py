880e9b781ca4a7449464d5f86058fba8901e9775
kohda <you@example.com>
2021-03-01 12:37:54 +0900

    --allow-empty-message

---
+++ ohm_max31865-test4.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+from datetime import datetime
+import spidev
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
+
+CS_list = [19,20,21]     # 使用するすべてのCSピンリスト
+abcs=int(sys.argv[1])    # 今使うCSピン(コマンドライン引数)
+
+def spisetup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(CS_list, GPIO.OUT)
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.no_cs = True    # spidevによるCSピンの自動制御を無効化
+    GPIO.output(CS_list, GPIO.HIGH)
+    spi.max_speed_hz = 1000000   # 1 MHz
+    spi.bits_per_word = 8
+    spi.mode = 1
+    GPIO.output(abcs, GPIO.LOW)
+    spi.xfer([0x80, 0b00010010])             # write configuration
+    GPIO.output(abcs, GPIO.HIGH)
+    return spi
+
+def spiread(spi, r_ref=430):
+    GPIO.output(abcs, GPIO.LOW)
+    spi.xfer([0x80, 0b10010010])             # v-bias on
+    GPIO.output(abcs, GPIO.HIGH)
+    sleep(0.01)                               # wait for RTDIN-capacitor full
+    GPIO.output(abcs, GPIO.LOW)
+    spi.xfer([0x80, 0b10110010])             # start 1-shot reading
+    GPIO.output(abcs, GPIO.HIGH)
+    sleep(0.01)                               # wait for saving to register
+    GPIO.output(abcs, GPIO.LOW)
+    bits = spi.xfer([0x01, 0x00, 0x00])[1:]  # read bits
+    GPIO.output(abcs, GPIO.HIGH)
+    GPIO.output(abcs, GPIO.LOW)
+    spi.xfer([0x80, 0b00010010])             # v-bias off
+    GPIO.output(abcs, GPIO.HIGH)
+    adc = bits[0] << 7 | bits[1] >> 1        # make 15 bits
+    r_rtd = 1.0* (adc * r_ref) / 2 ** 15 
+    return r_rtd
+
+def spiquit(spi):
+    spi.close()
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    spi = spisetup()
+    ohm = spiread(spi)
+    print('%.2f' % ohm)
+    spiquit(spi)
+    exit(0)
---
+++ ohm_to_celsius.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// http://seppina.cocolog-nifty.com/blog/2016/03/pt100-36de.html
+
+int main(int argc, char *argv[]){
+  float R0, R, dR, T;
+  R0 = 100;
+  R  = atof(argv[1]);
+  T  = (R/100 - 1)/ 3.9083e-3;
+  dR = R-R0*((((- 4.183e-12*T+4.183e-10)*T-5.775e-7)*T+3.9083e-3)*T+1.0);
+  T  = T+dR/R0/(((-4.183e-12*2.0*T+4.183e-10)*T-5.775e-7*2)*T+3.9083e-3);
+  dR = R-R0*((((- 4.183e-12*T+4.183e-10)*T-5.775e-7)*T+3.9083e-3)*T+1.0);
+  T  = T+dR/R0/(((-4.183e-12*2.0*T+4.183e-10)*T-5.775e-7*2)*T+3.9083e-3);
+
+  printf("%.2f\n",T);
+  return 0;
+}
